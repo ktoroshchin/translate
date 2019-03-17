f5aa4c40632f6c8631b2143b81b4ff747ecbfd65
ktoroshchcin <k.toroshchin@gmail.com>
2019-03-17 12:27:37 -0400

    Initial setup with semantic-ui and react

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render(){
+        return(
+            <div className="ui container">
+              <button  className="ui button">Heelooo, i am github</button>
+              <p className="ui header">how r u</p>
+            </div>
+        )
+    }
+}
+
+export default App;
